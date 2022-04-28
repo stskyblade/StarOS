5b59fd0cc2ae7927df3897c28e440d07a879b206
stskyblade <stskyblade@gmail.com>
2022-04-28 19:25:29 +0800

    feat: write to serial port

--- Makefile
+++ Makefile
@@ -4,10 +4,15 @@ BUILD=build
 RED=\033[0;32m
 NC=\033[0m # No Color
 
+obj= boot.o kernel.o
+
 kernel:
-	cd $(SRC) && i686-elf-as boot.s -o boot.o -g
-	cd $(SRC) && i686-elf-gcc -c kernel.c -o kernel.o -std=gnu99 -ffreestanding -O2 -Wall -Wextra -g
-	cd $(SRC) && i686-elf-gcc -T linker.ld -o ../$(BUILD)/myos.bin -ffreestanding -O2 -nostdlib boot.o kernel.o -lgcc -g
+	@cd $(SRC) && i686-elf-as boot.s -o boot.o -g
+	@cd $(SRC) && i686-elf-gcc -c kernel.c -o kernel.o -std=gnu99 -ffreestanding -O2 -Wall -Wextra -g
+	@cd $(SRC) && i686-elf-gcc -T linker.ld -o ../$(BUILD)/myos.bin -ffreestanding -O2 -nostdlib -lgcc -g $(obj)
+	@printf "${RED}Build kernel success!\n${NC}"
+
+iso: kernel
 	grub-file --is-x86-multiboot $(BUILD)/myos.bin && printf "${RED}Build kernel success.\n${NC}"
 	mkdir -p isodir/boot/grub
 	cp $(BUILD)/myos.bin isodir/boot/myos.bin
--- src/kernel.c
+++ src/kernel.c
@@ -13,6 +13,53 @@
 #error "This tutorial needs to be compiled with a ix86-elf compiler"
 #endif
 
+#define PORT 0x3f8 // COM1
+uint8_t *serial_port = (uint8_t *) PORT;
+
+static inline void outb(uint16_t port, uint8_t data) {
+    asm volatile("out %0,%1" : : "a"(data), "d"(port));
+}
+
+static inline uint8_t inb(uint16_t port) {
+    uint8_t data;
+
+    asm volatile("in %1,%0" : "=a"(data) : "d"(port));
+    return data;
+}
+
+static int init_serial() {
+    outb(PORT + 1, 0x00); // Disable all interrupts
+    outb(PORT + 3, 0x80); // Enable DLAB (set baud rate divisor)
+    outb(PORT + 0, 0x03); // Set divisor to 3 (lo byte) 38400 baud
+    outb(PORT + 1, 0x00); //                  (hi byte)
+    outb(PORT + 3, 0x03); // 8 bits, no parity, one stop bit
+    outb(PORT + 2, 0xC7); // Enable FIFO, clear them, with 14-byte threshold
+    outb(PORT + 4, 0x0B); // IRQs enabled, RTS/DSR set
+    outb(PORT + 4, 0x1E); // Set in loopback mode, test the serial chip
+    outb(PORT + 0, 0xAE); // Test serial chip (send byte 0xAE and check if
+                          // serial returns same byte)
+
+    // Check if serial is faulty (i.e: not same byte as sent)
+    if (inb(PORT + 0) != 0xAE) {
+        return 1;
+    }
+
+    // If serial is not faulty set it in normal operation mode
+    // (not-loopback with IRQs enabled and OUT#1 and OUT#2 bits enabled)
+    outb(PORT + 4, 0x0F);
+    return 0;
+}
+
+int is_transmit_empty() { return inb(PORT + 5) & 0x20; }
+
+void write_serial(char a) {
+    while (is_transmit_empty() == 0)
+        ;
+
+    outb(PORT, a);
+
+}
+
 /* Hardware text mode color constants. */
 enum vga_color {
     VGA_COLOR_BLACK = 0,
@@ -86,8 +133,10 @@ void terminal_putchar(char c) {
 }
 
 void terminal_write(const char *data, size_t size) {
-    for (size_t i = 0; i < size; i++)
-        terminal_putchar(data[i]);
+  for (size_t i = 0; i < size; i++) {
+      terminal_putchar(data[i]);
+      write_serial(data[i]);
+  }
 }
 
 void terminal_writestring(const char *data) {
@@ -97,6 +146,7 @@ void terminal_writestring(const char *data) {
 void kernel_main(void) {
     /* Initialize terminal interface */
     terminal_initialize();
+    init_serial();
 
     /* Newline support is left as an exercise. */
     terminal_writestring("Hello, World!\n");
